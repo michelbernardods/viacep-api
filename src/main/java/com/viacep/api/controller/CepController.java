3a868fac964cbb29e0d92319b8425690ed0638e1
Michel Bernardo <michel_13s@outlook.com>
2023-10-04 00:53:25 -0300

    feat: adding router for consuming cep

--- src/main/java/com/viacep/api/controller/CepController.java
+++ src/main/java/com/viacep/api/controller/CepController.java
@@ -1,5 +1,22 @@
 package com.viacep.api.controller;
 
+import com.viacep.api.model.Cep;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("consult-cep")
 public class CepController {
 
+    @GetMapping("{cep}")
+    public Cep consultCep(@PathVariable("cep") String cep) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Cep> response = restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json", cep), Cep.class);
+
+        return response.getBody();
+    }
 }
