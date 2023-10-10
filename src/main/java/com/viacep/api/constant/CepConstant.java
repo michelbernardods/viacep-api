a007e2cdb32fa2e964a22a625d0d0aa9dc4c0529
Michel Bernardo <michel_13s@outlook.com>
2023-10-09 22:58:11 -0300

    feat: adding messages package of constants

---
+++ src/main/java/com/viacep/api/constant/CepConstant.java
@@ -0,0 +1,13 @@
+package com.viacep.api.constant;
+
+public class CepConstant {
+    public static final String CEP_NOT_EXIST = "CEP is not exist ";
+    public static final String CEP_SEARCH = "Search cep in API ";
+    public static final String CEP_SAVED = "New Cep saved in database ";
+    public static final String CEP_DATABASE = "Get Cep in database ";
+    public static final String DATABASE_EMPTY = "Database with CEPs is empty";
+    public static final String CEP_TOTAL = "CEPs found. Total ";
+    public static final String CEP_TOTAL_DELETED = "All CEP deleted with success - Total:";
+
+
+}
