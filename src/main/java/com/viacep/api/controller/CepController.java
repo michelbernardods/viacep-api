7aa48884dde47f00be81e47f6f77704a3c0fb70c
Michel Bernardo <michel_13s@outlook.com>
2023-10-12 02:59:55 -0300

    refactor: modifying service and controller to have single responsibility

--- src/main/java/com/viacep/api/controller/CepController.java
+++ src/main/java/com/viacep/api/controller/CepController.java
@@ -1,17 +1,13 @@
 package com.viacep.api.controller;
 
-import com.viacep.api.api.Api;
 import com.viacep.api.model.Cep;
 import com.viacep.api.service.CepService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import static com.viacep.api.constant.CepConstant.*;
@@ -19,82 +15,53 @@ import static com.viacep.api.constant.CepConstant.*;
 @RestController
 @RequestMapping("v1/consult-cep")
 public class CepController {
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @Autowired
     private final CepService service;
-
     public CepController(CepService service) {
         this.service = service;
     }
-
     @GetMapping("{cep}")
-    public ResponseEntity<Object> consultCep(@PathVariable("cep") String cep) {
-        Optional<Cep> cepModel = service.findCepDB(cep);
-
-        if (cepModel.isEmpty()) {
-            Cep responseApi = service.findCepApi(cep);
-            if (responseApi.getCep() == null) {
-                logger.info(CEP_NOT_EXIST + cep);
-                return ResponseEntity.status(HttpStatus.OK).body(CEP_NOT_EXIST);
-            }
-
-            logger.info(CEP_SEARCH + cep);
-            logger.info(CEP_SAVED + cep);
-            return ResponseEntity.status(HttpStatus.CREATED).body(service.save(responseApi));
-
-        }
-
-        logger.info(CEP_DATABASE + cep);
-        return ResponseEntity.status(HttpStatus.OK).body(cepModel.get());
+    public ResponseEntity<Object> consultCep(@PathVariable("cep") String cep) throws Exception {
+        Cep result = service.findCepDB(cep);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
-
     @GetMapping
-    public ResponseEntity<Object> consultAllCepDataBase() {
-        List<Cep> cep = service.findAllCep();
-
-        if (cep.isEmpty()) {
-            logger.info(DATABASE_EMPTY);
-            return ResponseEntity.status(HttpStatus.OK).body(DATABASE_EMPTY);
+    public ResponseEntity<Object> consultAllCepDataBase() throws Exception {
+        try {
+            List<Cep> cep = service.findAllCep();
+            return ResponseEntity.status(HttpStatus.OK).body(cep);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
         }
-
-        logger.info(CEP_TOTAL + cep.size());
-        return ResponseEntity.status(HttpStatus.OK).body(cep);
     }
-
     @DeleteMapping
-    public ResponseEntity<Object> deleteAllCep() {
-        List<Cep> cep = service.findAllCep();
+    public ResponseEntity<Object> deleteAllCep() throws Exception {
+        try {
+            List<Cep> cep = service.findAllCep();
+            service.deleteAll();
+            return ResponseEntity.status(HttpStatus.OK).body(CEP_TOTAL_DELETED + cep.size());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
 
-        if (cep.isEmpty()) {
-            logger.info(DATABASE_EMPTY);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(DATABASE_EMPTY);
         }
-
-        service.deleteAll();
-        logger.info(CEP_TOTAL_DELETED);
-        return ResponseEntity.status(HttpStatus.OK).body(CEP_TOTAL_DELETED + cep.size());
     }
-
-@DeleteMapping("/id/{id}")
-    public ResponseEntity<Object> deleteUuid(@PathVariable("id") UUID id) {
-        Optional<Cep> cep = service.findById(id);
-
-        if (cep.isEmpty()) {
-            logger.info(CEP_NOT_EXIST);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(CEP_NOT_EXIST);
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<Object> deleteUuid(@PathVariable("id") UUID id) throws Exception {
+        try {
+            service.deleteCepByUuid(id);
+            return ResponseEntity.status(HttpStatus.OK).body(CEP_DELETED);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
         }
-
-        service.deleteCepByUuid(id);
-        logger.info(CEP_DELETED);
-        return ResponseEntity.status(HttpStatus.OK).body(CEP_DELETED);
     }
-
     @DeleteMapping("/cep/{cep}")
-    public ResponseEntity<Object> deleteCep(@PathVariable("cep") String cep) {
-        System.out.println(cep);
+    public ResponseEntity<Object> deleteCep(@PathVariable("cep") String cep) throws Exception {
+        try {
+            service.deleteCepByCep(cep);
+            return ResponseEntity.status(HttpStatus.OK).body(CEP_DELETED);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
+        }
 
-        service.deleteCepByCep(cep);
-        return ResponseEntity.status(HttpStatus.OK).body(CEP_DELETED);
     }
 }
--- src/main/java/com/viacep/api/service/CepService.java
+++ src/main/java/com/viacep/api/service/CepService.java
@@ -3,6 +3,8 @@ package com.viacep.api.service;
 import com.viacep.api.api.Api;
 import com.viacep.api.model.Cep;
 import com.viacep.api.repository.CepRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static com.viacep.api.constant.CepConstant.*;
 @Service
 public class CepService {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public Api api;
@@ -21,32 +26,62 @@ public class CepService {
     public Cep findCepApi(String cep) {
         return api.getCep(cep);
     }
-    public Optional<Cep> findCepDB(String cep) {
-        return repository.findByCep(cep);
+    public Cep findCepDB(String cep) throws Exception {
+
+       Optional<Cep> cepDB = repository.findByCep(cep);
+       if (cepDB.isEmpty()) {
+           Cep responseApi = findCepApi(cep);
+           if (responseApi.getCep() == null) {
+               logger.info(CEP_NOT_EXIST + cep);
+               throw new Exception(CEP_NOT_EXIST);
+           }
+
+           logger.info(CEP_SEARCH + cep);
+           logger.info(CEP_SAVED + cep);
+           save(responseApi);
+           return responseApi;
+       }
+
+        logger.info(CEP_DATABASE + cep);
+        return cepDB.get();
     }
 
-    public Optional<Cep> findById(UUID id) {
-        return repository.findById(id);
+    public void findById(UUID id) throws Exception {
+        Optional<Cep> cep =  repository.findById(id);
+        if (cep.isEmpty()) {
+            logger.info(CEP_NOT_EXIST);
+            throw new Exception(CEP_NOT_EXIST);
+        }
     }
-
 
     public void deleteAll() {
         repository.deleteAll();
     }
-    public void deleteCepByUuid(UUID id) {
+    public void deleteCepByUuid(UUID id) throws Exception {
+        findById(id);
         repository.deleteById(id);
     }
 
-    public void deleteCepByCep(String cep) {
-        Cep response = findCepDB(cep).get();
-        repository.deleteById(response.getUserId());
+    public void deleteCepByCep(String cep) throws Exception {
+        Optional<Cep> cepDB = repository.findByCep(cep);
+        if (cepDB.isEmpty()) {
+            logger.info(CEP_NOT_EXIST + cep);
+            throw new Exception(CEP_NOT_EXIST);
+        }
+
+        repository.deleteById(cepDB.get().getUserId());
     }
 
     public Object save(Cep cepModel) {
         return repository.save(cepModel);
     }
+    public List<Cep> findAllCep() throws Exception {
+        List<Cep> cep = repository.findAll();
+        if (cep.isEmpty()) {
+            logger.info(DATABASE_EMPTY);
+            throw new Exception(DATABASE_EMPTY);
+        }
 
-    public List<Cep> findAllCep() {
-        return repository.findAll();
+        return cep;
     }
 }
